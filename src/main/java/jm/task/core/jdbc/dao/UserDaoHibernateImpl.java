007044e57654a7c07c9b695bc221b13149d7f6c1
Yurik3 <105162903+Yurik3@users.noreply.github.com>
2023-01-07 03:16:45 +0300

    l

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -22,9 +22,7 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
-        Session session = null;
-        try {
-            session = factory.getCurrentSession();
+        try (Session session = factory.getCurrentSession()) {
             session.beginTransaction();
             session.createNativeQuery("CREATE TABLE  IF NOT EXISTS user " +
                             " ( Id BIGINT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255), lastName VARCHAR(255), age INT)")
@@ -33,74 +31,55 @@ public class UserDaoHibernateImpl implements UserDao {
             System.out.println("Таблица User создана");
         } catch (HibernateException e) {
             e.printStackTrace();
-        } finally {
-            session.close();
         }
 
     }
 
     @Override
     public void dropUsersTable() {
-        Session session = null;
-        try {
-            session = factory.getCurrentSession();
+        try (Session session = factory.getCurrentSession()) {
             session.beginTransaction();
             session.createNativeQuery("DROP TABLE  IF  EXISTS user ")
                     .executeUpdate();
             session.getTransaction().commit();
             System.out.println("Таблица User удалена");
-        } finally {
-            session.close();
         }
-
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-        Session session = null;
-        try {
-            session = factory.getCurrentSession();
+        try (Session session = factory.getCurrentSession()) {
             session.beginTransaction();
             session.save(new User(name, lastName, age));
             session.getTransaction().commit();
             System.out.println("User с именем – " + name + " добавлен");
         } catch (HibernateException e) {
             e.printStackTrace();
-        } finally {
-            session.close();
         }
     }
 
     @Override
     public void removeUserById(long id) {
-        Session session = null;
-        try {
-            session = factory.getCurrentSession();
+        try (Session session = factory.getCurrentSession()) {
             session.beginTransaction();
             session.delete(session.get(User.class, id));
             session.getTransaction().commit();
             System.out.println("User под номером – " + id + " удалён");
         } catch (HibernateException e) {
             e.printStackTrace();
-        } finally {
-            session.close();
         }
     }
 
     @Override
     public List<User> getAllUsers() {
         List<User> users = new ArrayList<>();
-        Session session = null;
-        try {
-            session = factory.getCurrentSession();
+        try (Session session = factory.getCurrentSession()) {
             session.beginTransaction();
             users = session.createQuery("from User").getResultList();
             for (User e : users)
                 System.out.println(e);
         } catch (HibernateException e) {
             e.printStackTrace();
-        } finally {
-            session.close();
         }
 
         return users;
@@ -108,9 +87,7 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void cleanUsersTable() {
-        Session session = null;
-        try {
-            session = factory.getCurrentSession();
+        try (Session session = factory.getCurrentSession()) {
             session.beginTransaction();
             session.createNativeQuery("TRUNCATE TABLE user")
                     .executeUpdate();
@@ -118,8 +95,6 @@ public class UserDaoHibernateImpl implements UserDao {
             System.out.println("Таблица User очищена");
         } catch (HibernateException e) {
             e.printStackTrace();
-        } finally {
-            session.close();
         }
 
 
